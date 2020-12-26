ac1ac5e79f3475d46a938ae0e8f96eb69a36e12b
Travis Brown <travis.brown3@bison.howard.edu>
2020-12-25 22:04:27 -0500

    link to home page

--- components/home_page.js
+++ components/home_page.js
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Home from './components/home_page';
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Home() {
   return (
-    // <View style = {styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-    <Home></Home>
+    <View style = {styles.container}>
+      <Text>Open up Home.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
