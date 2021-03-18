5fbb1f8c932f1190e33609a330cae61e4e37c7d8
Peniel Abebe <meetpenielo@gmail.com>
2021-03-18 10:35:41 -0400

    Landing page UI and routing.

--- hu-alum-app/src/App.js
+++ hu-alum-app/src/App.js
@@ -1,92 +1,26 @@
 import * as React from "react";
-import firebase from "firebase";
 import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer,
-  IfFirebaseAuthed,
-  IfFirebaseAuthedAnd
-} from "@react-firebase/auth";
-import { firebaseConfig } from "./firebaseConfig";
-import logo from './logo.svg';
-import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-export const App = () => {
+import profile from "./pages/profile";
+import landing from "./pages/landing";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+function App() {
   return (
-    <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
+    <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-      
-          <button
-            onClick={() => {
-              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-              firebase.auth().signInWithPopup(googleAuthProvider);
-            }}
-          >
-            Sign In with Google
-          </button>
-
-          {/* <button
-            data-testid="signin-anon"
-            onClick={() => {
-              firebase.auth().signInAnonymously();
-            }}
-          >
-            Sign In Anonymously
-          </button> */}
-
-          <button
-            onClick={() => {
-              firebase.auth().signOut();
-            }}
-          >
-            Sign Out
-          </button>
-
-          <FirebaseAuthConsumer>
-            {({ isSignedIn, user, providerId }) => {
-              return (
-                <pre style={{ height: 300, overflow: "auto" }}>
-                  {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
-                </pre>
-              );
-            }}
-          </FirebaseAuthConsumer>
-
-          <div>
-            <IfFirebaseAuthedAnd
-              filter={({ providerId }) => providerId !== "anonymous"}
-            >
-              {({ providerId }) => {
-                return <div>You are authenticated with {providerId}</div>;
-              }}
-            </IfFirebaseAuthedAnd>
-
-          </div>
+        <header>
+          <Switch>
+            <Route path="/" exact component={landing} />
+            <Route path="/profile" component={profile} />
+            <Route path="/login" component={login} />
+            <Route path="/signup" component={signup} />
+          </Switch>
         </header>
       </div>
-    </FirebaseAuthProvider>
+    </Router>
   );
-};
+}
+
 export default App;
---
+++ hu-alum-app/src/pages/signup.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function signupPage() {
+  return <h1>Sign up page in the making!</h1>;
+}
+export default signupPage;
