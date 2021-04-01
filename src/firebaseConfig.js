37e6f790fd3671fb81e15b11d777a039df7281b8
Travis Brown <travis.brown3@bison.howard.edu>
2021-04-01 10:12:49 -0400

    finalise firebase controls

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -14,30 +14,14 @@ var firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
-export { auth };
-export default db;
+export { auth, db };
+export default {db};
 
-export const getUserData = userUID => {
-  var docRef = db.collection("users").doc(userUID);
-
-  docRef.get().then((doc) => {
-      if (doc.exists) {
-          console.log("Document data:", doc.data());
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("There is no data for this user in our database");
-      }
-  }).catch((error) => {
-      console.log("Error getting document:", error);
-  });
-}
-
-export const setUserData = (userUID,fname, lname,email, phone_number, password) => {
+export const setUserData = (userUID,fname, lname,email, password) => {
   db.collection("users").doc(userUID).set({
       first_name: fname,
       last_name:lname,
       email: email,
-      phone_number: phone_number,
       password: password,
   })
   .then(() => {
