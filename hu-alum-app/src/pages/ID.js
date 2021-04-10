d777beab421ea24f2e9a613e0df6df1e91a3afc0
Tahj <tahjchisolm21@gmail.com>
2021-04-10 13:40:51 -0400

    inital ID json page

--- hu-alum-app/src/pages/ID.js
+++ hu-alum-app/src/pages/ID.js
@@ -1,6 +1,57 @@
-import React from "react";
+import * as React from "react";
+import firebase from "firebase";
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthedAnd,
+} from "@react-firebase/auth";
+import { firebaseConfig } from "../firebaseConfig";
+import "../App.css";
+import IDtemplate from "../IDtemplate.jpg";
+import QRtemplate from "../QRtemplate.jpg";
 
-function signupPage() {
-  return <h1>Membership ID Page coming soon!</h1>;
+
+
+
+function IDPage() {
+  return (
+    <div>
+      <header className="App-header">
+        <Member name="Name Here"/>
+        
+      
+        <IDCardFront />
+      </header>
+      
+             
+    </div>
+  
+  );
 }
-export default signupPage;
+
+function Member(member){
+  return (
+    <div>
+      <header>
+        <h1>Hello {member.name}!</h1>
+      </header>
+             
+    </div>
+  );
+}
+
+function IDCardFront() {
+  return (
+    <div>
+      <header >
+        <img src={IDtemplate} className="ID" />
+        <img src={QRtemplate} className="ID" />
+      </header>
+      
+             
+    </div>
+  
+  );
+}
+export default IDPage;
