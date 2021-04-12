fbdbe318232b001d3a28df87187fd4cfb9d2366b
Tahj <tahjchisolm21@gmail.com>
2021-04-12 19:31:58 -0400

    ID page ux layout

--- hu-alum-app/src/pages/ID.js
+++ hu-alum-app/src/pages/ID.js
@@ -10,44 +10,107 @@ import { firebaseConfig } from "../firebaseConfig";
 import "../App.css";
 import IDtemplate from "../IDtemplate.jpg";
 import QRtemplate from "../QRtemplate.jpg";
+import QRcode from "../fakeQRcode.png";
+import Avatar from "@material-ui/core/Avatar";
+import bison from "../bison.png";
+import { makeStyles } from "@material-ui/core/styles";
 
 
+const drawerWidth = 240;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // flexGrow: 1,
+    justify: "center",
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(6),
+    },
+  },
+  paper: {
+    height: 200,
+    width: 200,
+  },
+  // control: {
+  //   padding: theme.spacing(50),
+  // },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  menuButton: {
+    marginRight: theme.spacing(40),
+  },
+  hide: {
+    display: "none",
+  },
+  root1: {
+    display: "flex",
+    justify: "center",
+    marginLeft: 200,
+    "& > *": {
+      margin: theme.spacing(8),
+    },
+    marginTop: 50,
+  },
+}));
 
 function IDPage() {
+  const classes = useStyles();
   return (
     <div>
-      <header className="App-header">
-        <Member name="Name Here"/>
-        
-      
-        <IDCardFront />
-      </header>
-      
-             
+      <Avatar
+            alt="Remy Sharp"
+            src={bison}
+            className={classes.large}
+            style={{ height: "90px", width: "90px" }}
+          />
+    <h1>Howard University Alumni Member </h1>
+  
+    <div className="centering">
+    <IDCardFront align="center"/>
     </div>
+    
+      
+      <h3><Member name="Name Here"/></h3>
+      <h3>Membership ID: 999999999</h3>
+   
+  </div>
   
   );
 }
 
 function Member(member){
   return (
-    <div>
-      <header>
-        <h1>Hello {member.name}!</h1>
-      </header>
-             
-    </div>
+        <h3>Member Name: {member.name}!</h3>
   );
 }
 
 function IDCardFront() {
   return (
     <div>
-      <header >
-        <img src={IDtemplate} className="ID" />
-        <img src={QRtemplate} className="ID" />
-      </header>
+      
+        <img src={QRcode} className="QR" />
+      
       
              
     </div>
